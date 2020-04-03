604ad3072f6f33e883cf0892e1dcb49902d7241b
EmmaStolly <33861433+EmmaStolly@users.noreply.github.com>
2020-04-04 00:23:45 +0100

    MQTT Setup

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
+var mqtt = require("mqtt");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -10,6 +13,22 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+//Setup variables for the MQTT communication
+var MQTT_TOPIC = "/test";
+//MQTT Token
+var MQTT_ADDR = "mqtt://763f6861:1362154b898a179a@broker.shiftr.io";
+//Shiftr's port
+var MQTT_PORT = 1883;
+//Connect the client to the broker's address
+var mqttClient = mqtt.connect(MQTT_ADDR);
+
+//When the MQTT client is connected, subscribe to the topic
+mqttClient.on("connect", function() {
+  console.log("Connected to MQTT client");
+  //Subscribe to the MQTT topic
+  mqttClient.subscribe(MQTT_TOPIC, { qos: 1 });
 });
 
 const PORT = process.env.PORT || 3000;
